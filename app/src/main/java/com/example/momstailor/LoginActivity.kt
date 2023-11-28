210efb6a4251bc932e0d65523f3d9f5c74248434
Beluga <rohitcode005@gmail.com>
2023-11-28 12:36:07 +0530

    new UI updated by khushi rani

--- app/src/main/java/com/example/momstailor/LoginActivity.kt
+++ app/src/main/java/com/example/momstailor/LoginActivity.kt
@@ -1,6 +1,9 @@
 package com.example.momstailor
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,9 +16,21 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.momstailor.ui.theme.MomsTailorTheme
 
 class LoginActivity : ComponentActivity() {
+    lateinit var next:Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        next = findViewById(R.id.continue_btn)
+
+        next.setOnClickListener {
+            var intent:Intent = Intent(baseContext,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
 
 
     }
